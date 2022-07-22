3382322cf48efb226677e89634bb06aeabc77533
Lucas Loughner <69738550+LukeSev@users.noreply.github.com>
2022-07-22 11:41:11 -0700

    Done?

--- DiGraph.java
+++ DiGraph.java
@@ -8,6 +8,27 @@ public class DiGraph {
 
     private LinkedList<Integer>[] vertices;
 
+    private class VertexInfo {
+        int d, pred;
+
+        public VertexInfo(int d, int pred)
+        {
+            this.d = d;
+            this.pred = pred;
+        }
+    }
+
+    private class TreeNode {
+        int vert;
+        LinkedList<TreeNode> children;
+
+        public TreeNode(int vert, LinkedList<TreeNode> children)
+        {
+            this.vert = vert;
+            this.children = children;
+        }
+    }
+
     public DiGraph(int N)
     {
         this.vertices = new LinkedList[N];
@@ -121,6 +142,114 @@ public class DiGraph {
         if(ind != N)
             throw new IllegalArgumentException("Cyclic graph");
         return sorted;
+    }
+
+    private VertexInfo[] BFS(int s)
+    {
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        int N = this.vertexCount();
+        VertexInfo[] VA = new VertexInfo[N];
+
+        for(int i = 0; i < N; i++)
+            VA[i] = new VertexInfo(-1, -1);
+
+        VA[s-1].d = 0;
+        queue.addLast(s); // Queue uses natural indexing
+
+        while(!queue.isEmpty())
+        {
+            int u = queue.removeFirst() - 1;
+            for(int k = 0; k < this.vertices[u].size(); k++)
+            {
+                int v = this.vertices[u].get(k);
+                if(VA[v].d == -1)
+                {
+                    VA[v].d = VA[u].d + 1;
+                    VA[v].pred = u;
+                    queue.addLast(v+1);
+                }
+            }
+        }
+        return VA;
+    }
+
+    public boolean isTherePath(int from, int to)
+    {
+        VertexInfo[] VA = BFS(from);
+        if(VA[to-1].d != -1)
+            return true;
+        else
+            return false;
+    }
+
+    public int lengthOfPath(int from, int to)
+    {
+        int count = 0;
+        VertexInfo[] VA = BFS(from);
+        if(!this.isTherePath(from, to))
+            count = -1;
+
+        to -= 1;
+        while(VA[to].pred != -1)
+        {
+            count++;
+            to = VA[to].pred;
+        }
+
+        return count;
+    }
+
+    public void printPath(int from, int to)
+    {
+        VertexInfo[] VA = BFS(from);
+        String path;
+        if(VA[to-1].d == -1)
+            path = "No Path";
+        else {
+            path = "";
+            to -= 1;
+            while (from != to+1)
+            {
+                path = "-->" + (to + 1) + path;
+                to = VA[to].pred;
+            }
+            path = from + path;
+        }
+        System.out.println(path);
+    }
+
+    private TreeNode buildTree(int s)
+    {
+        VertexInfo[] VA = BFS(s);
+        int N = this.vertices.length;
+        int root;
+
+        TreeNode[] nodes = new TreeNode[N];
+        for(int i = 0; i < N; i++)
+            nodes[i] = new TreeNode(i, new LinkedList<TreeNode>());
+
+        for(int j = 0; j < VA.length; j++)
+        {
+            if(VA[j].pred != -1)
+                nodes[VA[j].pred].children.addLast(nodes[j]);
+        }
+        return nodes[s-1];
+    }
+
+    public void printTree(int s)
+    {
+        this.printTree_recursive(this.buildTree(s), 0);
+    }
+
+    private void printTree_recursive(TreeNode node, int level)
+    {
+        for(int i = 0; i < level; i++)
+            System.out.print("    ");
+        System.out.println(node.vert+1);
+
+        int numchildren = node.children.size();
+        for(int j = 0; j < numchildren; j++)
+            printTree_recursive(node.children.get(j), level+1);
     }
 
 }
--- DiGraphTest.java
+++ DiGraphTest.java
@@ -12,6 +12,7 @@ public class DiGraphTest {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter number of vertices: ");
         int N = in.nextInt();
+        in.nextLine();
 
         DiGraph dg = new DiGraph(N);
 
@@ -22,58 +23,62 @@ public class DiGraphTest {
         System.out.println("- vertex count (enter v)");
         System.out.println("- print graph (enter p)");
         System.out.println("- topological sort (enter t)");
+        System.out.println("- is there a path between vertices (enter i)");
+        System.out.println("- find length of shortest path between vertices (enter l)");
+        System.out.println("- print shortest path between vertices (enter s)");
+        System.out.println("- print breadth-first-tree from vertex (enter b)");
         System.out.println("- Quit (enter q)");
 
         int quit = 0;
         while(quit != 1)
         {
             System.out.println("Enter choice of operation:");
-
-            char choice = in.next().charAt(0);
             String inLine = in.nextLine();
 
-            if(inLine.length() > 0)
+            if(inLine.length() > 1)
             {
                 System.out.println("Invalid operation");
             }
             else {
-                switch (choice) {
-                    case 'a':
+                switch (inLine) {
+                    case "a":
                         System.out.println("Add edge from vertex v to vertex u in following format: 'v u'");
                         int v = in.nextInt();
                         int u = in.nextInt();
+                        in.nextLine();
                         if (dg.addEdge(v, u) == 1)
                             System.out.printf("Edge (%d,%d) was added%n", v, u);
                         else
                             System.out.printf("Edge (%d,%d) already exists%n", v, u);
                         break;
 
-                    case 'd':
+                    case "d":
                         System.out.println("Delete edge from vertex v to vertex u in following format: 'v u'");
                         v = in.nextInt();
                         u = in.nextInt();
+                        in.nextLine();
                         if (dg.deleteEdge(v, u) == 1)
                             System.out.printf("Edge (%d,%d) was deleted%n", v, u);
                         else
                             System.out.printf("Edge (%d, %d) does not exist%n", v, u);
                         break;
 
-                    case 'e':
+                    case "e":
                         int edges = dg.edgeCount();
                         System.out.printf("Edge count: %d%n", edges);
                         break;
 
-                    case 'v':
+                    case "v":
                         int vertex_count = dg.vertexCount();
                         System.out.printf("Vertex count: %d%n", vertex_count);
                         break;
 
-                    case 'p':
+                    case "p":
                         System.out.println("The graph is the following:");
                         dg.print();
                         break;
 
-                    case 't':
+                    case "t":
                         try
                         {
                             int[] sorted = dg.topSort();
@@ -89,8 +94,47 @@ public class DiGraphTest {
                         }
                         break;
 
-                    case 'q':
+                    case "i":
+                        System.out.println("Enter two vertices to see if there's a path to/from (in form to from)");
+                        int from = in.nextInt();
+                        int to = in.nextInt();
+                        in.nextLine();
+                        boolean is = dg.isTherePath(from, to);
+                        if(is)
+                            System.out.println("There is a path between the vertices");
+                        else
+                            System.out.println("There is NOT a path between the vertices");
+                        break;
+
+                    case "l":
+                        System.out.println("Enter vertices to find length of path from/to:");
+                        from = in.nextInt();
+                        to = in.nextInt();
+                        in.nextLine();
+
+                        int count = dg.lengthOfPath(from, to);
+                        System.out.printf("Length of path from %d to %d: %d%n", from, to, count);
+                        break;
+
+                    case "s":
+                        System.out.println("Enter vertices to print path from/to:");
+                        from = in.nextInt();
+                        to = in.nextInt();
+                        in.nextLine();
+                        dg.printPath(from, to);
+                        break;
+
+                    case "q":
+                        System.out.println("Goodbye");
                         quit = 1;
+                        break;
+
+                    case "b":
+                        System.out.println("Enter vertex to build tree from:");
+                        int root = in.nextInt();
+                        in.nextLine();
+                        System.out.println("Breadth-First-Tree:");
+                        dg.printTree(root);
                         break;
 
                     default:
@@ -98,11 +142,7 @@ public class DiGraphTest {
                         break;
                 }
             }
-
         }
-
-
     }
-
 
 }
